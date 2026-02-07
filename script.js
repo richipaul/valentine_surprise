1350922595e826f98185ba71b78a1318f5642499
Richi paul <richipaul8f25@gmail.com>
2026-02-07 21:46:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function showMessage(day) {
 
     const messages = [
-        "🌹 Just like this rose, my feelings for you are pure and real.",
-        "🤝 I may not say it every day, but I choose you.",
-        "🍫 Life is sweeter with you in it.",
-        "🧸 You are my comfort and my peace.",
-        "🤝 I promise to stand by you always.",
-        "🤗 Sending you the warmest hug.",
-        "😘 One kiss from you can fix anything.",
-        "❤️ You are my today and all my tomorrows."
+        "🌹Happy Rose Day to the girl who makes my life look like a garden. Just like a rose, you're beautiful and bright, but it’s your heart that really stands out. Love you, Anisha❤️!",
+        "🤝 I don't need a ring to tell you this, but I’ll choose you every single day. Thanks for being the best teammate I could ever ask for. Will you keep being my person forever?",
+        "🍫 To my sweetest Anisha: You’re the 'sugar rush' that gets me through my toughest days. Thanks for always being so helpful and making life taste a lot better.",
+        "🧸 You’re the cutest person I know, but you're also my safest place to land. Sending you a virtual squeeze today until I can give you a real one!",
+        "🤝 I promise to always be the person who hypes you up the way you do for me. I promise to be by your side, just like you’ve always been by mine. You’re my support system, Anisha.",
+        "🤗Whenever I’m stressed, one hug from you fixes everything. Thanks for being the peace in my chaos. Can't wait to hold you tight today",
+        "😘 A million kisses to the girl who believes in me even when I don’t believe in myself. You make me feel like I can conquer the world",
+        "❤️ Happy Valentine’s Day, Anisha! You’re more than just my girlfriend; you’re my best friend and my rock. Thank you for always hyping me up and staying by my side. I’m so lucky you're mine."
     ];
 
     document.getElementById("messageBox").innerText = messages[day];
