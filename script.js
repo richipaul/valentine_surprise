66f2ee7127b0c489e84b4c7c285a59c107b1621d
Richi paul <richipaul8f25@gmail.com>
2026-02-07 21:07:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage(day) {
+
+    const messages = [
+        "🌹 Just like this rose, my feelings for you are pure and real.",
+        "🤝 I may not say it every day, but I choose you.",
+        "🍫 Life is sweeter with you in it.",
+        "🧸 You are my comfort and my peace.",
+        "🤝 I promise to stand by you always.",
+        "🤗 Sending you the warmest hug.",
+        "😘 One kiss from you can fix anything.",
+        "❤️ You are my today and all my tomorrows."
+    ];
+
+    document.getElementById("messageBox").innerText = messages[day];
+}
